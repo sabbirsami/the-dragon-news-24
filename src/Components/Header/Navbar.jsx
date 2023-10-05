ec2b979576d1e7f3580d7aa9452c613eb3899a9c
SabbirSami <smd71430@gmail.com>
2023-10-05 13:14:40 +0600

    navbar done

--- src/Components/Header/Navbar.jsx
+++ src/Components/Header/Navbar.jsx
@@ -1,9 +1,47 @@
-
+import { useState } from "react";
+import { NavLink } from "react-router-dom";
+import userImg from "../../assets/user.png";
 
 const Navbar = () => {
+    const [user, setUser] = useState(false);
     return (
-        <div>
-            
+        <div className="container mx-auto">
+            <div className="flex justify-between items-center">
+                <div className="">
+                    <nav>
+                        <ul className="flex gap-5">
+                            <li>
+                                <NavLink className={"pb-3"} to={"/"}>
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink className={"pb-3"} to={"/about"}>
+                                    About
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink className={"pb-3"} to={"/career"}>
+                                    Career
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <div className="">
+                    {user ? (
+                        <img
+                            className="w-10 h-auto mt-2"
+                            src={userImg}
+                            alt=""
+                        />
+                    ) : (
+                        <button className="bg-rose-700 text-white  py-2 mt-2 px-8  ">
+                            <NavLink to={"/login"}>login</NavLink>
+                        </button>
+                    )}
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,13 @@
 import BreakingNews from "../Header/BreakingNews";
 import Header from "../Header/Header";
+import Navbar from "../Header/Navbar";
 
 const Home = () => {
     return (
         <div>
             <Header />
             <BreakingNews />
+            <Navbar />
         </div>
     );
 };
